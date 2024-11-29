5cb9a7f0581501af2f150936f761d8def5fa9373
SergeyReizman <sergeytlv1971@gmail.com>
2024-11-29 17:22:56 +0200

    +comments

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+// Wait for the entire DOM content to be loaded and parsed before executing the code
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio loaded!');
-  });
-  
+  // Logs a message to the console indicating that the portfolio page has successfully loaded
+  console.log('Portfolio loaded!'); 
+
+  // Additional JavaScript functionality can be added here
+  // For example, setting up event listeners or initializing dynamic features
+});
